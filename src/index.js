bac747cbb1d6b451d4307838ea199ac1753ca6cb
bartcevich <bartcevich@tut.by>
2023-02-21 22:09:59 +0300

    start6

--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@
 function towelSort (matrix){
     let arr=[];
     for (let i = 0; i < matrix.length; i++){
-      matrix[i].sort((a,b)=>a-b);
-      for (let a = 0; a < matrix[i].length; a++){
-        arr.push(matrix[i][a]);
-      } 
+      if(i%2===0){
+        for (let a = 0; a < matrix[i].length; a++){
+          arr.push(matrix[i][a]);
+         } 
+      }else{
+        for (let a = matrix[i].length-1; a > 0 ; a--){
+          arr.push(matrix[i][a]);
+        }
+      }
     } 
-    arr.sort((a,b)=>a-b);
     return arr;
   }
 
